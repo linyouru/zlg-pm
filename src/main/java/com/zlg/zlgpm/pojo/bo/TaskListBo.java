8c30f29b5462dab5d4726230d8bb4f4e428abeba
linyouru1 <linyouru@zlg.cn>
2022-11-04 10:49:31 +0800

    补漏

---
+++ src/main/java/com/zlg/zlgpm/pojo/bo/TaskListBo.java
@@ -0,0 +1,222 @@
+package com.zlg.zlgpm.pojo.bo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+
+/**
+ * 任务列表实体
+ * @author linyouru
+ */
+public class TaskListBo {
+
+    private Integer id;
+    private String projectName;
+    private String projectVersion;
+    private String taskType;
+    private String task;
+    private String status;
+    private String nickName;
+    private String playStartTime;
+    private String playEndTime;
+    private String startTime;
+    private String endTime;
+    private String timely;
+    private String quality;
+    private String document;
+    private String remark;
+    private String link;
+    private String updateTime;
+    private String createTime;
+    private String overtime;
+    private String warning;
+
+    public TaskListBo() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectVersion() {
+        return projectVersion;
+    }
+
+    public void setProjectVersion(String projectVersion) {
+        this.projectVersion = projectVersion;
+    }
+
+    public String getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(String taskType) {
+        this.taskType = taskType;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getPlayStartTime() {
+        return playStartTime;
+    }
+
+    public void setPlayStartTime(String playStartTime) {
+        this.playStartTime = playStartTime;
+    }
+
+    public String getPlayEndTime() {
+        return playEndTime;
+    }
+
+    public void setPlayEndTime(String playEndTime) {
+        this.playEndTime = playEndTime;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getTimely() {
+        return timely;
+    }
+
+    public void setTimely(String timely) {
+        this.timely = timely;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getOvertime() {
+        return overtime;
+    }
+
+    public void setOvertime(String overtime) {
+        this.overtime = overtime;
+    }
+
+    public String getWarning() {
+        return warning;
+    }
+
+    public void setWarning(String warning) {
+        this.warning = warning;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskListBo{" +
+                "id=" + id +
+                ", projectName='" + projectName + '\'' +
+                ", projectVersion='" + projectVersion + '\'' +
+                ", taskType='" + taskType + '\'' +
+                ", task='" + task + '\'' +
+                ", status='" + status + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", playStartTime='" + playStartTime + '\'' +
+                ", playEndTime='" + playEndTime + '\'' +
+                ", startTime='" + startTime + '\'' +
+                ", endTime='" + endTime + '\'' +
+                ", timely='" + timely + '\'' +
+                ", quality='" + quality + '\'' +
+                ", document='" + document + '\'' +
+                ", remark='" + remark + '\'' +
+                ", link='" + link + '\'' +
+                ", updateTime='" + updateTime + '\'' +
+                ", createTime='" + createTime + '\'' +
+                ", overtime='" + overtime + '\'' +
+                ", warning='" + warning + '\'' +
+                '}';
+    }
+}
