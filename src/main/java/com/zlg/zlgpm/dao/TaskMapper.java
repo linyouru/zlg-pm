d3ee5ef84b8bb56260e6a9b96331c686b029a512
linyouru1 <linyouru@zlg.cn>
2023-03-28 11:38:45 +0800

    Revert "修复GET /v1/task/list 排序字段moduleLevel报错"
    This reverts commit 5082842802d638b174a7906532d8e7b97f439ff6.

--- src/main/java/com/zlg/zlgpm/dao/TaskMapper.java
+++ src/main/java/com/zlg/zlgpm/dao/TaskMapper.java
@@ -36,7 +36,7 @@ public interface TaskMapper extends BaseMapper<TaskPo> {
             "         t.remark,\n" +
             "         t.link,\n" +
             "         pm.module,\n" +
-            "         pm.`level` AS moduleLevel,\n" +
+            "         pm.level,\n" +
             "         t.haveDocument,\n" +
             "         t.updateTime,\n" +
             "         t.createTime,\n" +
