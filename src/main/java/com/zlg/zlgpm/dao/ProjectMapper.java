f2972ca2226f3d3396a7a9eeb19b94e963ebc03e
linyouru1 <linyouru@zlg.cn>
2023-04-04 17:24:13 +0800

    GET /v1/project/list 返回数据不带version

--- src/main/java/com/zlg/zlgpm/dao/ProjectMapper.java
+++ src/main/java/com/zlg/zlgpm/dao/ProjectMapper.java
@@ -26,7 +26,7 @@ public interface ProjectMapper extends BaseMapper<ProjectPo> {
     /**
      * mybatis-plus 使用 Wrapper 自定义SQL,查询项目管理列表数据
      */
-    @Select("SELECT p.id, p.`name`, pv.version, p.uid, u.nickName, p.`status`, p.remark, p.updateTime, p.createTime FROM `project` AS `p` LEFT JOIN `user` AS `u` ON p.uid = u.id LEFT JOIN project_version AS pv ON p.id = pv.pid ${ew.customSqlSegment}")
+    @Select("SELECT p.id, p.`name`, p.uid, u.nickName, p.`status`, p.remark, p.updateTime, p.createTime FROM `project` AS `p` LEFT JOIN `user` AS `u` ON p.uid = u.id ${ew.customSqlSegment}")
     Page<ProjectBo> selectPageByName(Page<ProjectBo> projectBoPage, @Param(Constants.WRAPPER) Wrapper<ProjectBo> ew);
 
     @Select("SELECT `name` FROM `project` ${ew.customSqlSegment}")
