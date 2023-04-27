12da727c0c385dc4f1a4c57616e378cb5f132c49
linyouru1 <linyouru@zlg.cn>
2023-04-27 09:06:35 +0800

    时间精度修改

--- src/main/resources/sql/projectTable.sql
+++ src/main/resources/sql/projectTable.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS `project`
     `status`     varchar(8)  NOT NULL COMMENT '1开发中；2已完成',
     `remark`     varchar(255)                           DEFAULT NULL COMMENT '备注',
     `link`       varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `updateTime` datetime(3) NOT NULL                   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    `createTime` datetime(3) NOT NULL                   DEFAULT CURRENT_TIMESTAMP,
+    `updateTime` datetime NOT NULL                   DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `createTime` datetime NOT NULL                   DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE KEY `index_n` (`name`) USING BTREE
 ) ENGINE = InnoDB
