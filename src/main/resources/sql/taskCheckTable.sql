eb2e1a30b5f06f3d3152ebfdd54cdea5ec34a235
linyouru1 <linyouru@zlg.cn>
2023-07-14 10:31:23 +0800

    修改DDL

--- src/main/resources/sql/taskCheckTable.sql
+++ src/main/resources/sql/taskCheckTable.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS `task_check`
 (
     `id`         int(11) NOT NULL AUTO_INCREMENT,
-    `taskId`      int(11)    DEFAULT NULL COMMENT '任务id',
+    `taskId`     int(11)    DEFAULT NULL COMMENT '任务id',
     `type`       tinyint(4) DEFAULT NULL COMMENT '1验收，2问题',
     `isTimely`   tinyint(4) DEFAULT '0' COMMENT '1及时,2不及时',
     `createTime` datetime   DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    KEY `index_taskId` (`taskId`) USING BTREE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
